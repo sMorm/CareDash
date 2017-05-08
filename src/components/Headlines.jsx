55a772bfba8bf7b9061d9a7bcd6feb74f2d06c80
sMorm <sereymorm@gmail.com>
2017-05-08 15:49:23 -0400

    Completed Headlines with SVGs

---
+++ src/components/Headlines.jsx
@@ -0,0 +1,40 @@
+// Dependencies
+import React, { Component } from 'react';
+
+// Styles
+import './styles/Headlines.css'
+
+// Media
+import Medicine from './media/medicine.svg';
+import Heart from './media/heart.svg';
+import Clipboard from './media/clipboard.svg';
+
+class Headlines extends Component {
+  render(){
+    return(
+      <div className="HeadlineContainer">
+      <h1> See why others love CareDash </h1>
+        <div className="HeadlineFlexContainer">
+          <span>
+            <img src={Medicine} alt="PrescriptionBottle"/>
+            <h3>Manage Prescriptions</h3>
+            <p>Lorem sit amet, consectetur adipiscing elit, sed do eiusmod tempor ut labore et dolore magna aliqua. </p>
+          </span>
+          <span>
+            <img src={Heart} alt="Heart with Cross in Center"/>
+            <h3>Active Lifestyle</h3>
+            <p>Dolor ipsum dolor , consectetur adipiscing elit, sed do eiusmod tempor ut labore et dolore. </p>
+          </span>
+          <span>
+            <img src={Clipboard} alt="Animated Clipboard"/>
+            <h3>Staying on Track</h3>
+            <p>Ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor ut labore et dolore magna aliqua. </p>
+          </span>
+        </div>
+        <hr />
+      </div>
+    )
+  }
+}
+
+export default Headlines;
--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,9 +1,79 @@
+// Dependencies
 import React, { Component } from 'react';
+import Typist from 'react-typist';
 
+// Components
+import Headlines from './Headlines.jsx'
+
+// Styles
+import './styles/Home.css'
+import './styles/Typist.css'
+
+// Media
+import SearchIcon from './media/searchicon.svg'
 class Home extends Component {
+  constructor(){
+    super();
+    this.onSubmit = this.onSubmit.bind(this);
+    this.onChange = this.onChange.bind(this);
+    this.state= {
+      location: '',
+      specialty: ''
+    }
+  }
+  onSubmit(event){
+    event.preventDefault();
+    const { location, specialty } = this.state
+    console.log(`API Request for ${specialty} in ${location}`)
+  }
+  onChange(event){
+    this.setState({
+      [event.target.name]: event.target.value
+    })
+  }
   render() {
     return (
-      <p>Home</p>
+      <div>
+        <div className="HomeHeader">
+          <div className="HomeText">
+            <Typist 
+            className="HomeText Typist"
+            cursor={{ 
+              hideWhenDone: true,
+              hideWhenDoneDelay: 7000,
+              stdTypingDelay: 50,
+              avgTypingDelay: 100
+            }}>
+              Find, Compare, and Review Doctors
+            </Typist>
+            <p>Search by name, specialty, and location to get honest and transparent information, ratings, and reviews on your health care providers.</p>
+            <div className="SearchContainer">
+              <form onSubmit={this.onSubmit} className="SearchForm">
+                <input 
+                  className="SearchForm left"
+                  type="text"
+                  name="location"
+                  autoComplete="off"
+                  placeholder="Address or Zipcode"
+                  onChange={this.onChange}
+                  onSubmit={this.onSubmit}
+                  required/>
+                <input 
+                  className="SearchForm right"
+                  type="text"
+                  name="specialty"
+                  autoComplete="off"
+                  placeholder="Doctor Name, Specialty, or Hospital"
+                  onChange={this.onChange}
+                  onSubmit={this.onSubmit}
+                  required/>
+                <button><img src={SearchIcon} alt="Search Button"/></button>
+              </form>
+            </div>
+          </div>
+        </div>
+        <Headlines />
+      </div>
     );
   }
 }
